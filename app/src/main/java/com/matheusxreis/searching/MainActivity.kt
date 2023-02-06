3e73df2f6aa49ae67df06fa07ad97f463b765802
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 14:41:33 -0300

    working in activity

--- app/src/main/java/com/matheusxreis/searching/MainActivity.kt
+++ app/src/main/java/com/matheusxreis/searching/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.matheusxreis.searching
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val btn = findViewById<Button>(R.id.button)
+        btn.setOnClickListener {
+           //
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+
+
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if(item.itemId == R.id.search_item){
+            onSearchRequested()
+        }
+
+        return true
+    }
+
+
 }
---
+++ app/src/main/java/com/matheusxreis/searching/SearchResult.kt
@@ -0,0 +1,11 @@
+package com.matheusxreis.searching
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchResult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result)
+    }
+}
